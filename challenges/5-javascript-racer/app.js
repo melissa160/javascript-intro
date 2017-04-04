f66bae75d81fa9ee4767b71f780c4098223cc5f4
melissa160 <melissa160@gmail.com>
2017-04-03 19:53:55 -0500

    update ruby racer

--- challenges/5-javascript-racer/app.js
+++ challenges/5-javascript-racer/app.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
-  var count1 = 0
-  var count2 = 0
-  var trackLength = 45
+
   $(document).on('keyup', function(event) {
     //jugador 1
     
     if(event.keyCode == 81){        
-      if (count1 < trackLength-1 ){
-        $($('#player1_strip td')[count1]).removeClass("active");
-        $('#player1_strip').append("<td></td>")
-        $($('#player1_strip td')[count1+1]).addClass("active");
-        count1++;
-      } else {
-        console.log("el jugador 1 gano")
-      }
+      count1 = pushForward(count1, "player1")
     }
 
     //jugador 2
     if(event.keyCode == 80){
-        if (count2 < trackLength-1 ){
-        $($('#player2_strip td')[count2]).removeClass("active");
-        $('#player2_strip').append("<td></td>")
-        $($('#player2_strip td')[count2+1]).addClass("active");
-        count2++;
-      } else {
-        console.log("el jugador 2 gano")
-      } 
+      count2 = pushForward(count2, "player2")
     }
+    console.log(count1)
+    console.log(count2)
 
   });
 });
+
+var count1 = 0
+var count2 = 0
+var trackLength = 15
+
+function pushForward(count, player){
+  if (count < trackLength-1 ){
+    $($(`#${player}_strip td`)[count]).toggleClass("active");
+    $(`#${player}_strip`).append("<td class='active'></td>")
+    count++;
+  } else {
+    console.log(`el ${player} gano`)
+  }
+  return count
+}
