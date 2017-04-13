15a5cd9b45f81d80a8b2f0613d3ef0823d5e2c9f
melissa160 <melissa160@gmail.com>
2017-04-13 11:21:41 -0500

    implentamentando handlebars

--- challenges/ajax/1-todos/app/controllers/index.rb
+++ challenges/ajax/1-todos/app/controllers/index.rb
@@ -1,7 +1,57 @@
+# index
 get '/' do
+  redirect '/todos'
+end
+
+get '/todos' do
+  @all_todos = Todo.all
   erb :index
 end
 
-post '/add_todo' do
-  p "Dentro de la ruta  /add_todo!"
+#create
+post '/todos' do
+  # p '*'*50
+  todo = Todo.new(todo_content: params[:content])
+  if todo.save
+    # erb :"_todo", layout: false, locals: {todo: todo}
+    content_type :json
+    {id: todo.id, todo_content: todo.todo_content }.to_json
+  else
+
+  end
+end
+
+#new
+
+#show
+
+#edit
+get "/todos/:id/edit" do
+  @todo = Todo.find_by(id: params[:id]) 
+  erb :edit
+end
+
+#update
+patch "/todos/:id" do
+  p '*'*50
+  p "en patch"
+  todo = Todo.find_by(id: params[:id])
+  todo.update(todo_content: params[:content])
+  redirect "/todos"
+end
+
+
+#delete
+delete "/todos/:id" do
+  p '*'*50
+  p "en delete"
+  todo = Todo.find_by(id: params[:id])
+  todo.delete
+  content_type :json
+  {id: todo.id}.to_json
+end
+
+get '/todos/:id' do
+  todos = Todos.find(params[:id])
+  erb :"_todo", layout: false, locals: {todo: todo}
 end
--- challenges/ajax/1-todos/public/js/application.js
+++ challenges/ajax/1-todos/public/js/application.js
@@ -2,14 +2,27 @@ $(document).ready(function() {
   bindEvents();
 });
 
-
 function bindEvents() {
   // Enlaza los eventos que agregan, remuevan y completan TODOS a los elmentos del DOM indicados
+  $('form.addTodo').on('submit', addTodo);
+  $(".todo_list").on("click", "a.delete", deleteTodo);
+  $('form.edit').on('submit', editTodo);
+
+}
+
+//HANDLEBAR
+var source = $("#entry_template").html();
+console.log(source)
+var template = Handlebars.compile(source)
+var persona = {
+  "nombre": "hola",
+  "como": "como",
 }
 
 function buildTodo(todoName) {
   // Eso nos ta un pedazo del DOM
   var todoTemplate = $.trim($('#todo_template').html());
+  console.log(todoTemplate)
   // Creamos un elemento de jquery a partir del template
   var $todo = $(todoTemplate);
   // Modificamos el texto con el que nos pasen como argumento
@@ -19,3 +32,48 @@ function buildTodo(todoName) {
 }
 
 // Crea las funciones que añaden, remueven y completan TODOS. 
+function addTodo(event){
+  event.preventDefault()
+  console.log("en add")
+  var url = this.action
+  var data = $(this).serialize()
+  $.ajax({
+    method: "POST",
+    url: url,
+    data: data,
+  }).done(function(response){
+    console.log(response)
+    var todo = template(response)
+    console.log(todo)  
+    $("#todo_template").html(todo)
+    // debugger
+    // buildTodo(response.todo_template)    
+    // $(".todo_list").append(todo)
+  }).fail()
+}
+
+function deleteTodo(event){
+  event.preventDefault()
+  console.log("en delete")
+  var deleteElement = this
+  var url = deleteElement.href
+  $.ajax({
+    method: "DELETE",
+    url: url,
+  }).done(function(response){
+    console.log(response)
+    $(deleteElement).closest('article').remove()
+
+  }).fail()
+}
+
+function editTodo(event){
+  // event.preventDefault()
+  console.log("en edit")
+}
+
+
+
+
+var html = template(persona)
+$(".container").html(html)
